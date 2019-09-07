3113e5b2ef6e60ca76e3cdb3dcc56189824d2fa7
YaroslavDrozdovskiy <54240910+YaroslavDrozdovskiy@users.noreply.github.com>
2019-09-07 18:14:19 +1000

    Add files via upload

---
+++ js/game.js
@@ -0,0 +1,70 @@
+$(document).ready(function () {
+  const maxHits = 10;
+
+  let hits = 0;
+  let miss = 0;
+  let firstHitTime = 0;
+  let timer = false; // переменная ,отражающая состояние запуска таймера
+  let starter = false; //переменная отражающая состояние запуска игры
+
+  function round() {
+    let divSelector = randomDivId();
+    $(divSelector).addClass("target");
+    $(".target").text(hits);
+
+    if (!timer) {
+      firstHitTime = getTimestamp();
+      timer = true;
+    }
+    if (hits === maxHits) {
+      endGame();
+    }
+  }
+
+  function endGame() {
+    $(".slot-wrap").hide();
+    let totalPlayedMillis = getTimestamp() - firstHitTime;
+    let totalPlayedSeconds = Number(totalPlayedMillis / 1000).toPrecision(3);
+    $("#total-time-played").text(totalPlayedSeconds);
+    $("#total-miss").text(miss);
+    $("#win-message").removeClass("d-none");
+    if (miss > 0) {
+      $("#total-miss").append(`\nСтарайтесь лучше!`);
+    }
+    $("#button-start").addClass('d-none')
+
+  }
+
+  function handleClick(event) {
+    // FIXME: убирать текст со старых таргетов. Кажется есть .text?
+    if ($(event.target).hasClass("target")) {
+      hits += 1;
+      $(event.target).text('');
+      $(event.target).removeClass("target");
+      $('.miss').text('');
+      $('*').removeClass("miss");
+      round();
+    }
+    else {
+      $(event.target).addClass("miss");
+      miss += 1;
+      $(event.target).text(randomMissText);
+    }
+  }
+
+  $("#button-start").click(function () {
+    if (!starter) {
+      round();
+      starter = true;
+    }
+    $(".game-field").click(handleClick);
+    $(".game-field").click(timer);
+  })
+
+  $("#button-reload").click(function () {
+    location.reload();
+  });
+});
+
+
+
---
+++ js/utils.js
@@ -0,0 +1,17 @@
+function getTimestamp() {
+  let d = new Date();
+  return d.getTime();
+}
+
+function randomDivId() {
+  let d = Math.floor(Math.random() * 4);
+  let n = Math.floor(Math.random() * 6) + 1;
+  return `#slot-${d}${n}`;
+}
+
+function randomMissText() {
+  let missText = ['Упс:(', 'Промазал..', 'Мимо!'];
+  random = Math.floor(Math.random() * missText.length);
+  console.log(missText[random]);
+  return missText[random];
+}
